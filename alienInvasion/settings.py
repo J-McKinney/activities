d237bdb8d12d26c44af587ed663bdc233377a895
Jesse McKinney <jessman51386@gmail.com>
2020-02-26 19:43:50 -0500

    yay alien invasion

---
+++ alienInvasion/settings.py
@@ -0,0 +1,18 @@
+class  Settings:
+    """A class to store all the settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen Settings
+        # display 1300px wide and 700px high
+        self.screen_width = 1300
+        self.screen_height = 700
+        # Set the background color
+        self.bg_color = (230, 230, 230)
+        # Ship settings
+        self.ship_speed = 1.5
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (250, 60, 60)
