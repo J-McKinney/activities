844b17954d8fcd1001fc84f8c5ea6add18e3c1bf
Jesse McKinney <jessman51386@gmail.com>
2020-03-06 15:48:07 -0500

    plotting the starting and ending points, page 321

--- dataVisualization/rw_visual.py
+++ dataVisualization/rw_visual.py
@@ -13,6 +13,10 @@ while True:
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Reds, edgecolors='none', s = 15)
 
+    # Emphasize the first and last points
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='blue', edgecolors='none', s=100)
+
     plt.show()
 
     keep_running = input("Make another walk? (Y / N): ")
